3cab3223351f774df5296f7865fc78975d54af95
Ezequiel Lagos <ezequiel.lagos@alumnos.upla.cl>
2017-06-28 00:59:47 -0400

    Ezequiel: se obtienen datos de UPLA, falta gusrdar en BD

--- scraper/upla.py
+++ scraper/upla.py
@@ -1,59 +1,47 @@
 from bs4 import BeautifulSoup
 import urllib
 import feedparser
+import unicodedata
+
+def elimina_tildes(s):
+   return ''.join((c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn'))
 
 url_rss = "http://www.upla.cl/noticias/feed/"
 feed = feedparser.parse( url_rss )
 
 for item in feed['items']:
-    print item['date']
-    print item['title']
-    print item['summary']
-    print item['link']
+    #print item['date']
+    titulo =  item['title']
+    bajada =  item['summary']
+    link = item['link']
+    categoria = item['category']
+    fecha = item['published']
+
+
+    print categoria
+    print titulo
+    print bajada
+    print link
+    print fecha
+
+    # Parsea la categoria para ser buscada
+    categoria_busqueda = categoria.lower()
+    categoria_busqueda = elimina_tildes(categoria_busqueda)
+    categoria_busqueda = categoria_busqueda.replace(" ", "-")
+
+    # Entra en la pagina de cada categoria y busca todas las noticias
+    contents = urllib.urlopen("http://www.upla.cl/noticias/category/"+categoria_busqueda+"/").read()
+    bs = BeautifulSoup(contents, "html.parser")
+    articles = bs.find_all("article", ["item-list"])
+
+    # Por cada noticia obtiene su titulo
+    for article in articles:
+        titulo_articulo = article.find("a").text
+
+        # Si el titulo de la noticia es igual al titulo obtenido del XML, obtiene la imagen de esa noticia y termina el ciclo
+        if titulo_articulo == titulo:
+            imagen = article.find("img")['src']
+            break
+
+    print imagen
     print "---------------------------------------------------------"
-
-
-#contents = urllib.urlopen("http://www.upla.cl/noticias/feed/").read()
-#bs = BeautifulSoup(contents, "xml")
-
-#print bs
-
-
-"""
-divs = bs.find_all("div", ["item n_caja borde6", "item n_caja borde6 fin"])
-
-for div in divs:
-    fecha = div.find("div", ["fecha"]).text
-    link = div.a['href']
-    link = "http://www.uv.cl/pdn" + link.replace("..", "")
-
-    # Accede a la pagina de la noticia
-    pagina_noticia = urllib.urlopen(link).read()
-    bs_noticia = BeautifulSoup(pagina_noticia, "html.parser")
-    titulo = bs_noticia.find("div", id = "n_titulo").text
-    bajada = bs_noticia.find("div", id = "n_bajada").text
-    imagen = bs_noticia.find("div", id = "n_clipex")
-
-    video = imagen.iframe
-    imagen = imagen.img
-    if video is not None:
-        # Se obtiene el video de portada de la noticia
-        video = video.get('src')
-        #print video
-    else:
-        # Se obtiene la imagen de portada de la noticia
-        imagen = imagen.get('src')
-        imagen = "http://www.uv.cl" + imagen
-
-    noticias = bs_noticia.find("div", id="n_cuerpo")
-    noticias = noticias.find_all("p")
-
-    for noticia in noticias:
-        noticia.find("div").clear()
-        print noticia
-
-
-
-    #print titulo, "|" ,titulo2
-    print "--------------------------------------------------------------------------------------------"
-"""
