eacc7df71f666e978e6fde207a7ce564358bcf6e
Ezequiel Lagos <ezequiel.lagos@alumnos.upla.cl>
2017-07-05 02:28:57 -0400

    Se añade admin

--- UniNews/news/admin.py
+++ UniNews/news/admin.py
@@ -2,5 +2,17 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Universidad, Noticias
 
 # Register your models here.
+
+class UniversidadAdmin(admin.ModelAdmin):
+	fields = ['id_universidad', 'nombre','region']
+	list_display = ('id_universidad', 'nombre','region')
+
+class NoticiasAdmin(admin.ModelAdmin):
+	fields = ['id_noticia', 'titulo','bajada', 'link_noticia', 'link_recurso', 'categoria', 'contador_visitas', 'id_universidad']
+	list_display = ('id_noticia', 'titulo','bajada', 'link_noticia', 'link_recurso', 'categoria', 'contador_visitas', 'id_universidad')
+
+admin.site.register(Universidad, UniversidadAdmin)
+admin.site.register(Noticias, NoticiasAdmin)
