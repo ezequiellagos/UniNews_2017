ba33627bb8ee4241f89c2856a360a728de7fac8a
Ezequiel Lagos <ezequiel.lagos@alumnos.upla.cl>
2017-07-05 02:28:40 -0400

    Se añaden urls

--- UniNews/news/urls.py
+++ UniNews/news/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<id_noticia>[0-9]+)/$', views.detalle, name='detalle'),
+    url(r'^estadisticas/$' , views.estadisticas, name='estadisticas'),
+    url(r'^region/(?P<region>[0-9]+)$' , views.region, name='region'),
+    url(r'^categoria/(?P<categoria>[0-9A-Za-z]+)$' , views.categoria, name='categoria'),
 ]
